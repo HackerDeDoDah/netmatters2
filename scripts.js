31a0538bd0f301fc7f8fdf6286d55f54b114737e
Chris Day <chrisdaypro@protonmail.com>
2024-11-10 15:54:55 +0000

    side bar css and js added

---
+++ scripts.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "300px";
+    document.getElementById("main").style.marginRight = "300px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
+}
