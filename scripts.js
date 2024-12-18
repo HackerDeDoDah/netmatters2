25ef2d1b34f5f50e7c6779c9b8a3b80e331f88f4
Chris Day <chrisdaypro@protonmail.com>
2024-12-18 15:49:03 +0000

    js for partner carousel added. works but needs work.

--- scripts.js
+++ scripts.js
@@ -6,3 +6,88 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginRight= "0";
 }
+
+
+// partner carousel top -----------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselWrapper = document.querySelector(".carousel-wrapper");
+    const images = document.querySelectorAll(".carousel-wrapper img");
+    const totalImages = images.length;
+    const imageWidth = images[0].offsetWidth + 15;
+    let scrollPosition = 0;
+
+    function scrollCarousel() {
+        scrollPosition -= 300;
+        if (Math.abs(scrollPosition) >= (totalImages / 2) * imageWidth) {
+            carouselWrapper.style.transition = "none";
+            scrollPosition = 1200;
+            carouselWrapper.style.transform = `translateX(${scrollPosition}px)`;
+
+            setTimeout(() => {
+                carouselWrapper.style.transition = "transform 0.5s ease-in-out";
+            }, 50);
+        } else {
+            carouselWrapper.style.transform = `translateX(${scrollPosition}px)`;
+        }
+    }
+
+    function startCarousel() {
+        interval = setInterval(scrollCarousel, 5000);
+    }
+
+    function stopCarousel() {
+        clearInterval(interval);
+    }
+
+    let interval = setInterval(scrollCarousel, 3000);
+
+    carouselWrapper.addEventListener("mouseover", stopCarousel);
+    carouselWrapper.addEventListener("mouseout", startCarousel);
+});
+
+
+
+
+// carousel partners bottom ------------------
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carouselWrapper2 = document.querySelector(".carousel-wrapper2");
+    const items = document.querySelectorAll(".tool-tip");
+    const itemWidth = items[0].offsetWidth + 15;
+    let scrollPosition = 0;
+    let interval;
+
+    function scrollCarousel() {
+        scrollPosition -= 200;
+        
+        if (Math.abs(scrollPosition) >= carouselWrapper2.scrollWidth) {
+            carouselWrapper2.style.transition = "none";
+            scrollPosition = 0;
+            carouselWrapper2.style.transform = `translateX(${scrollPosition}px)`;
+
+            setTimeout(() => {
+                carouselWrapper2.style.transition = "transform 0.5s ease-in-out";
+            }, 50);
+        } else {
+            carouselWrapper2.style.transform = `translateX(${scrollPosition}px)`;
+        }
+    }
+
+    function startCarousel() {
+        interval = setInterval(scrollCarousel, 5000);
+    }
+
+    function stopCarousel() {
+        clearInterval(interval);
+    }
+
+    items.forEach(item => {
+        item.addEventListener("mouseover", stopCarousel);
+        item.addEventListener("mouseout", startCarousel);
+    });
+
+    startCarousel();
+});
+
+
