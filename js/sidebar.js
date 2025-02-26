096d93617bbbf5408e7264c8c65b8651b9651bc7
Chris Day <chrisdaypro@protonmail.com>
2025-02-26 16:58:36 +0000

    hamburger working again

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,40 +2,46 @@
 
 function toggleNav() {
     var body = document.querySelector(".full-container");
+    var burger = document.querySelector(".hamburger"); // Select hamburger element
 
     var isOpen = body.style.transform === "translateX(-360px)";
 
     if (!isOpen) {
         openNav();
+        burger.classList.add("open"); // Add open class
     } else {
         closeNav();
+        burger.classList.remove("open"); // Remove open class
     }
 }
+
 
 function openNav() {
     var body = document.querySelector(".full-container");
     var overlay = document.querySelector(".overlay");
     var screenWidth = window.innerWidth;
-    var sidebarWidth = screenWidth < 450 ? 275 : 360; // Adjusted sidebar width for smaller screens
+    var sidebarWidth = screenWidth < 450 ? 275 : 360;
 
     body.style.transform = `translateX(-${sidebarWidth}px)`;
     body.style.transition = "transform 0.3s ease";
     overlay.style.display = "flex";
 
-    document.dispatchEvent(new Event("sidebarOpen")); // Notify sticky.js
-    isSidebarOpen = true; // Update sidebar state
+    document.dispatchEvent(new Event("sidebarOpen")); // tell sticky.js
+    isSidebarOpen = true; // Update state
 }
 
 function closeNav() {
     var body = document.querySelector(".full-container");
     var overlay = document.querySelector(".overlay");
+    var burger = document.querySelector(".hamburger"); 
 
     body.style.transform = "translateX(0)";
     overlay.style.display = "none";
 
-    document.dispatchEvent(new Event("sidebarClose")); // Notify sticky.js
-    isSidebarOpen = false; // Update sidebar state
+    burger.classList.remove("open"); // hamburger reset
 
+    document.dispatchEvent(new Event("sidebarClose")); // Notify
+    isSidebarOpen = false;
 }
 
 
@@ -47,7 +53,7 @@ document.querySelector(".overlay").addEventListener('click', function() {
 });
 
 
-// Swipe-to-close on Mobile
+// Swipe-to-close
 let startX = 0;
 let sidebar = document.getElementById("mySidebar");
 
